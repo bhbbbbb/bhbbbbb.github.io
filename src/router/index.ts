d7fbaf19b0e7dd44ec7c5b65d765e22aecb29761
smark <smark9005310@gmail.com>
2026-03-08 13:26:46 +0800

    initial commit

---
+++ src/router/index.ts
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '../pages/HomePage'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomePage,
+    },
+    // {
+    //   path: '/:albumName/:rotation(\\d)?/:offset(\\d+)?',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    // },
+  ],
+})
+
+export default router

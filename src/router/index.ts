b49705dd0388a4fe824a2672f86b4b635316917d
smark <smark9005310@gmail.com>
2026-04-08 18:19:47 +0800

    chore: add spa fallback in ci

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,14 +14,6 @@ const router = createRouter({
       name: 'note',
       component: () => import('../pages/NotePage.vue'),
     },
-    // {
-    //   path: '/:albumName/:rotation(\\d)?/:offset(\\d+)?',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
-    // },
   ],
 })
 
